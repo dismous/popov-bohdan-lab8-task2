6ae24e6f2e3cd56c62b8461b44063a24792aa166
dismous <bohdanpopov9@gmail.com>
2023-11-05 15:32:19 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -3,6 +3,8 @@ def validate_board(board):
     result = True
     matrix = []
     tmp_list = []
+    color_y = []
+    color_x = []
     for i in range(9):
         temp_array = []
         for j in range(len(board[i])):
@@ -22,15 +24,36 @@ def validate_board(board):
             lin = []
     tmp_list = []
     for k in range(9):
-        tmp_list.append(a[k,:])                             #column
+        tmp_list.append(a[k,:])                             #line
         for l in tmp_list:
             for t in l:
-                if (t.isdigit()) and (int(t) <= 9) and (int(t) >= 1):
+                if (t.isdigit()) and 0 <= int(t) <= 9:
                     if t in lin:
                         result =  False
+                        break
                     else:
                         lin.append(t)
             lin = []
+    
+    for i in range(9):
+        color_y.append(a[:, i].tolist())
+        color_x.append(a[8 - i, :].tolist())
+        color_x = color_x[0]
+        color_y = color_y[0]
+        yellow = color_x[i:(5 + i)]
+        for j in color_y[4-i:9-i]:
+            yellow.append(j)
+        dub = []
+        for k in yellow:
+            if k.isdigit() and 0 <= int(k) <= 9:
+                if k in dub:
+                    result = False
+                    break
+                else:
+                    dub.append(k)
+        color_y = []
+        color_x = []
+
     return result
 
 bor = [
@@ -40,7 +63,7 @@ bor = [
  "* 4 1****",
  "     9 5 ",
  " 6  83  *",
- "3   1  **",
+ "3   2  **",
  "  8  2***",
  "  2  ****"
 ]
